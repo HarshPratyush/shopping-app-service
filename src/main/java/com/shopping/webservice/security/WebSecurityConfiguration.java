1668f75343c7d7af66c8464e142292adbe113f91
Harsh Pratyush <harshpratyush1206@gmail.com>
2020-11-21 23:30:33 +0530

    Added

---
+++ src/main/java/com/shopping/webservice/security/WebSecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.shopping.webservice.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+}
